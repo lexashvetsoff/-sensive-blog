3f97f2d3dc56a5cbb3548d69ec847217a436a42f
lexashvetsoff <lexa.shvetsoff@yandex.ru>
2022-07-14 14:42:22 +0800

    Отделен STATIC_ROOT от STATICFILES_DIRS

--- sensive_blog/settings.py
+++ sensive_blog/settings.py
@@ -42,7 +42,7 @@ TEMPLATE_DIR = os.path.join(BASE_DIR, 'templates')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATIC_ROOT = ''
+STATIC_ROOT = os.path.join(BASE_DIR, 'assets')
 
 TEMPLATES = [
     {
